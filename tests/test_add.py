1a2b1d44722bde00d33f4d697806a77708a2dd81
bin913 <842884726@qq.com>
2026-07-11 17:28:51 +0800

    fix code-format

--- tests/test_add.py
+++ tests/test_add.py
@@ -11,6 +11,7 @@ import flag_dnn
 from tests import consts
 from tests import accuracy_utils as utils
 
+
 def _cudnn_add(x, y, cudnn_handle):
     graph = cudnn_graph(x.dtype, cudnn_handle)
     x_tensor = graph.tensor_like(x)
